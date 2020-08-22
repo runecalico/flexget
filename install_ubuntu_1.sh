195fec410137e65b89547c224a811f881b9c1a67
Jonathan Kaufman <webreg@tir-nan-og.com>
2020-08-22 09:48:18 -0500

    Install scripts for ubuntu

---
+++ install_ubuntu_1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Ubuntu 20.04 LTS
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
+sudo apt-get install python3-venv
+sudo apt-get install libtorrent-dev # Needed for convert_magnet
+python3 -m venv ~/flexget/
+# Add this to .bash_aliases
+# alias flexgetSource='source ~/flexget/bin/activate'
---
+++ install_ubuntu_2.sh
@@ -0,0 +1,6 @@
+# Ubuntu 20.04LTS
+# Run in your virtual ENV to install flexget  
+pip3 install wheel
+pip3 install deluge-client # Needed to talk to Deluge
+pip3 install lbry-libtorrent # Needed for convert_magnet
+pip3 install flexget
