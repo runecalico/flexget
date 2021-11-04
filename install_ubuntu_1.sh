e9ef874d93279fcedf75301b998e999170606662
Jonathan Kaufman <webreg@tir-nan-og.com>
2021-11-04 16:19:33 -0500

    Sync - Update flexget to latest

--- install_ubuntu_1.sh
+++ install_ubuntu_1.sh
@@ -3,6 +3,8 @@
 sudo apt-get install python3-pip
 sudo pip3 install --upgrade pip
 sudo apt-get install python3-venv
+sudo apt-get install python3-distutils
+sudo apt-get install python3-dev
 sudo apt-get install libtorrent-dev # Needed for convert_magnet
 python3 -m venv ~/flexget/
 # Add this to .bash_aliases
--- install_ubuntu_2.sh
+++ install_ubuntu_2.sh
@@ -1,5 +1,8 @@
 # Ubuntu 20.04LTS
-# Run in your virtual ENV to install flexget  
+# Run in your virtual ENV to install flexget
+# source ~/flexget/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
 pip3 install wheel # to upgrade: pip3 install --upgrade deluge-client
 pip3 install deluge-client # Needed to talk to Deluge - to upgrade: pip3 install --upgrade deluge-client
 pip3 install lbry-libtorrent # Needed for convert_magnet - to upgrade: pip3 install --upgrade lbry-libtorrent
