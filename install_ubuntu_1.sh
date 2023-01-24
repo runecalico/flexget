83cc8fc37e2531c2db5b313362cfc0d6d7cd166a
runecalico <18315837+runecalico@users.noreply.github.com>
2023-01-24 08:00:11 -0600

    2012-01-24 Sync

--- install_ubuntu_1.sh
+++ install_ubuntu_1.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
-# Ubuntu 20.04 LTS
-# sudo apt-get install python3-pip
-# sudo pip3 install --upgrade pip
-# sudo apt-get install python3-venv
-# sudo apt-get install python3-distutils
-# sudo apt-get install python3-dev
-# sudo apt-get install libtorrent-dev # Needed for convert_magnet
-# python3 -m venv ~/flexget/
+# Ubuntu 20.04 LTS (python3 is 3.8.x)
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
+sudo apt-get install python3-venv
+sudo apt-get install python3-distutils
+sudo apt-get install python3-dev
+sudo apt-get install rename
+sudo apt-get install libtorrent-dev # Needed for convert_magnet
+python3 -m venv ~/flexget/
 # Add this to .bash_aliases
 # alias flexgetSource='source ~/flexget/bin/activate'
 
@@ -15,5 +16,6 @@
 sudo apt-get install python3-venv
 sudo apt-get install python3-distutils
 sudo apt-get install python3-dev
+sudo apt-get install rename
 sudo apt-get install libtorrent-dev # Needed for convert_magnet
 python3 -m venv ~/flexget/
